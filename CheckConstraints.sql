2b9c45fbe18c2ad8602aa2b9c50ec21f870cbf3f
mngola <mikambingongola@yahoo.com>
2018-03-01 23:29:24 -0500

    Data Modification document

--- CheckConstraints.sql
+++ CheckConstraints.sql
@@ -1,4 +1,4 @@
-alter table sponsorships add add constraint sponsorships_amount_check check (amount>0);
+alter table sponsorships add constraint sponsorships_amount_check check (amount>0);
 
 alter table guests add constraint speciality check 
 (speciality = 'speaker' or 
