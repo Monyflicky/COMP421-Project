9d30a5e98c2af4754063bd632856f6dc0d1f3492
mngola <mikambingongola@yahoo.com>
2018-03-01 20:20:35 -0500

    final create statements

--- CreateStatements.sql
+++ CreateStatements.sql
@@ -1,14 +1,19 @@
-create table room (
-roomno varchar,
-capacity int,
-CONSTRAINT valid_room CHECK (roomno ~* '^(N|S|W|E).*'),
-primary key(roomno)
+create table activity (
+expatt int not null,
+agelim int,
+actname varchar,
+reqstafff int not null,
+duration time not null,
+primary key(actname)
 );
 
-create table equipment (
-eq_name varchar, 
-eq_id varchar,
-primary key(eq_id)
+create table attendance(
+	username varchar,
+	sdate date,
+	actname varchar, 
+	foreign key (username) references attendee(username),
+	foreign key (sdate, actname) references schedactivity(sdate, actname) ON UPDATE CASCADE ON DELETE CASCADE,
+	primary key(username, sdate, actname)
 );
 
 create table attendee (
@@ -23,35 +28,10 @@ username varchar,
 primary key(username)
 );
 
-create table rentals(
+create table equipment (
+eq_name varchar, 
 eq_id varchar,
-actname varchar not null,
-primary key(eq_id),
-foreign key(eq_id) references equipment(eq_id),
-foreign key(actname) references activity(actname) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-create table sponsorships (
-actname varchar,
-sname varchar,
-amount integer not null,
-primary key(actname),
-foreign key(actname) references activity(actname) ON UPDATE CASCADE ON DELETE CASCADE,
-foreign key(sname) references sponsor(sname),
-);
-
-create table sponsor(
-	sname VARCHAR not null,
-	contrib integer not null,
-	primary key (sname)	
-);
-
-create table staff(
-	sid integer not null,
-	stname varchar not null,
-	role varchar,
-	hours integer not null,
-	primary key(sid)
+primary key(eq_id)
 );
 
 create table guests(
@@ -60,6 +40,57 @@ create table guests(
 	country varchar,
 	speciality varchar not null, 
 	primary key(gname, speciality)
+);
+
+CREATE TABLE liasons (
+	sid int NOT NULL,
+	sname varchar NOT NULL,
+	FOREIGN KEY (sid) REFERENCES staff(sid),
+	FOREIGN KEY (sname) REFERENCES sponsor(sname),
+	primary key(sid,sname)
+);
+
+CREATE TABLE Market (
+	expatt INTEGER NOT NULL,
+	agelim integer,
+	actname varchar UNIQUE,
+	reqstaff integer NOT NULL,
+	duration time NOT NULL,
+	FOREIGN KEY (actname) REFERENCES activity(actname) ON UPDATE CASCADE ON DELETE CASCADE,
+	primary key(actname)
+);
+
+CREATE TABLE Panel (
+	expatt INTEGER NOT NULL,
+	agelim integer,
+	actname varchar UNIQUE,
+	reqstaff integer NOT NULL,
+	duration time NOT NULL,
+	FOREIGN KEY (actname) REFERENCES activity(actname) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE Performers (
+	gname varchar,
+	actname varchar,
+	speciality varchar,
+	FOREIGN KEY (gname, speciality) REFERENCES guests(gname, speciality),
+	FOREIGN KEY (actname) REFERENCES shows(actname) ON UPDATE CASCADE ON DELETE CASCADE,
+	CHECK (speciality = 'performer')
+);
+
+create table rentals(
+eq_id varchar,
+actname varchar not null,
+primary key(eq_id),
+foreign key(eq_id) references equipment(eq_id),
+foreign key(actname) references activity(actname) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+create table room (
+roomno varchar,
+capacity int,
+CONSTRAINT valid_room CHECK (roomno ~* '^(N|S|W|E).*'),
+primary key(roomno)
 );
 
 create table schedactivity(
@@ -81,11 +112,53 @@ create table schedule(
 	primary key(sid, roomno, sdate, actname)
 );
 
-create table attendance(
-	username varchar,
-	sdate date,
-	actname varchar, 
-	foreign key (username) references attendee(username),
-	foreign key (sdate, actname) references schedactivity(sdate, actname) ON UPDATE CASCADE ON DELETE CASCADE,
-	primary key(username, sdate, actname)
+CREATE TABLE Shows (
+	expatt INTEGER NOT NULL,
+	agelim integer,
+	actname varchar UNIQUE,
+	reqstaff integer NOT NULL,
+	duration time NOT NULL,
+	FOREIGN KEY (actname) REFERENCES activity(actname)
+);
+
+CREATE TABLE Speakers (
+	gname varchar,
+	actname varchar,
+	speciality varchar,
+	FOREIGN KEY (gname, speciality) REFERENCES guests(gname, speciality),
+	FOREIGN KEY (actname) REFERENCES panel(actname) ON UPDATE CASCADE ON DELETE CASCADE,
+	CHECK (speciality = 'speaker')
+);
+
+create table sponsor(
+	sname VARCHAR not null,
+	contrib integer not null,
+	primary key (sname)	
+);
+
+create table sponsorships (
+actname varchar,
+sname varchar,
+amount integer not null,
+primary key(actname),
+foreign key(actname) references activity(actname) ON UPDATE CASCADE ON DELETE CASCADE,
+foreign key(sname) references sponsor(sname),
+);
+
+create table staff(
+	sid integer not null,
+	stname varchar not null,
+	role varchar,
+	hours integer not null,
+	primary key(sid)
+);
+
+CREATE TABLE Vendors (
+	gname varchar,
+	actname varchar,
+	speciality varchar,
+	FOREIGN KEY (gname, speciality) REFERENCES guests(gname, speciality),
+	FOREIGN KEY (actname) REFERENCES panel(actname) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY (gname, speciality),
+	CHECK (speciality = 'vendor')
 );
